f2e1fad1ce56441df8ee4571e5496cb896d3bb08
Isuru Adikaram <izuru775@gmail.com>
2021-08-30 14:25:25 +1000

    test added to check api functionality

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect;
+var request = require('request');
+
+describe("Test get post api",function(){
+    var url = "http://localhost:3000/api/projects";
+    // To test if the api exists 
+    it("return status 200 to check if api works",function(done){
+        request(url,function(error,response,body){
+            expect(response.statusCode).to.equal(200);
+            done()
+        });
+    });
+    // To test if the api is working correctly
+    it("return statusCode key in the body to check if api give right result should be 200",function(done){
+        request(url,function(error,response,body){
+            body = JSON.parse(body)
+            expect(body.statusCode).to.equal(200);
+            done()
+        });
+    });
+
+
+});
