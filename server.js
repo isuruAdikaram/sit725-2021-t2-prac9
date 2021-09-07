bfe069420f70fcd9066dd784a2dac4e78b99b823
Isuru Adikaram <izuru775@gmail.com>
2021-09-07 18:07:11 +1000

    ejs views created

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const indexRoute =require('./routes/projects')
 const dbConnection = require('./dbConnect')
 const cors = require("cors")
 const path = require('path')
+const expressLayouts = require('express-ejs-layouts')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 
 const app = express();
 
@@ -11,27 +15,34 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use(express.json());
+// Express body parser 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
+// EJS
+app.use(expressLayouts)
+app.set('view engine','ejs')
+
+app.use(session({
+  secret:'cat',
+  resave:true,
+  saveUninitialized:true
+}))
+
+// Connect flash
+app.use(flash())
+
+// Global Vars
+app.use((req,res,next)=>{
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  next()
+})
 
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
-const itemsList =[
-  {
-    title:"Cowardly-cat",
-    image:"assets/cat-1.jpg",
-    link:"stupidcats.com",
-    description:"wacked birds all day"
-  },
-  {
-    title:"Enlightened-cat",
-    image:"assets/cat-2.jpg",
-    link:"evenstupidercats.com",
-    description:"harased dogs all day"
-  }
-]
 
 var port = process.env.PORT || 3000;
 
