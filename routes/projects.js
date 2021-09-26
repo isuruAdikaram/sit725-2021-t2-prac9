4348a83dc9080d3f04b2a81626d28ed46f7f0cf3
Isuru Adikaram <izuru775@gmail.com>
2021-09-27 05:40:59 +1000

    Logic is seperated from the routes

--- routes/projects.js
+++ routes/projects.js
@@ -7,192 +7,50 @@ const router = express.Router()
 const Controllers = require('../controllers')
 const passport = require('passport')
 
+// Route for loading pet data 
 router.get('/', ensureAuthenticated, (req, res) => {
   Controllers.projectController.getProjects(req,res)
 
 })
-
+// Create pet route
 router.post('/', ensureAuthenticated, (req, res) => {
-  // console.log("New Project added", req.body)
   Controllers.projectController.createProjects(req, res)
 
 })
-
 // User logout
 router.get('/logout', (req, res) => {
-  req.logout()
-  req.flash('success_msg', 'You are logged out')
-  res.redirect('/users/login')
+  Controllers.projectController.logoutController(req,res)
 })
 // Login route
 router.get('/login', (req, res, next) => {
-  res.render('login', { title: "Login" })
+  Controllers.projectController.loginController(req,res,next)
 })
 // Login route
 router.post('/login', (req, res, next) => {
-  console.log(req.body)
-  passport.authenticate('local', function (err, user, info) {
-    console.log(info)
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-
-      return res.redirect('/users/login');
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect('/');
-    });
-
-  })(req, res, next);
+  Controllers.projectController.loginAuthenticationController(req,res,next)
 })
-
 // Register route
 router.get('/register', (req, res) => {
-  res.render('register', { title: "register" })
+  Controllers.projectController.registerController(req,res)
 })
 // Register router
 router.post('/register', (req, res) => {
-  const { firstName, lastName, email, password, password2 } = req.body;
-  console.log(req.body)
-  let errors = [];
-
-  if (!firstName || !lastName || !email || !password || !password2) {
-    errors.push({ msg: "Please fill all the fields" })
-  }
-
-  if (password !== password2) {
-    errors.push({ msg: "Password incorrect" })
-  }
-
-  if (password.length < 6) {
-    errors.push({ msg: "Password should be at least 6 characters" })
-  }
-
-  if (errors.length > 0) {
-    res.render('register', {
-      title: "Register",
-      firstName,
-      lastName,
-      email,
-      password,
-      password2,
-      errors
-    })
-  }
-  else {
-    User.findOne({ email: email })
-      .then(user => {
-        if (user) {
-          errors.push({ msg: " Email already registered" })
-          res.render('register', {
-            title: "Register",
-            firstName,
-            lastName,
-            email,
-            password,
-            password2,
-            errors
-          })
-        } else {
-
-          const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password
-          });
-
-          // Hash password
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) {
-                throw err
-              }
-              // Set password to hash
-              newUser.password = hash;
-              // Save usr
-              newUser.save()
-                .then(user => {
-                  req.flash('success_msg', 'You are now registered and can log in')
-                  res.redirect('/login')
-                })
-                .catch(err => console.log(err))
-            })
-          })
-        }
-      })
-  }
+  Controllers.projectController.registerPostController(req,res)
 })
 // Pet delete router
 router.delete('/delete', (req, res) => {
-  const { id } = req.body;
-  console.log(id)
-  Pet.deleteOne({ _id: id })
-    .then(record => {
-      if (record) {
-        res.json({ msg: "success!!!" })
-      } else {
-        res.json({ msg: "Record dosen't exist!" })
-      }
-    })
-    .catch(err => console.log(err));
+  Controllers.projectController.deleteController(req,res)
 })
+// Pet update router
 router.put('/upload', (req, res) => {
-  console.log(req.body)
-  const { id, title, image, link, description } = req.body
-
-  Pet.findByIdAndUpdate({ _id: id }, { $set: { title, image, link, description } })
-    .then(result => {
-      if (result) {
-        res.json({ statusCode: 200, msg: "Pet record updated" })
-      } else {
-        res.json({ statusCode: 400, msg: "Unable to find record" })
-      }
-    })
-    .catch(err => console.log(err))
+  Controllers.projectController.updateController(req,res)
 })
 // Get user account 
 router.get('/account',ensureAuthenticated,(req,res)=>{
-  User.findOne({_id:req.user._id})
-    .then(user=>{
-      if(user){
-        let firstName = user.firstName
-        let lastName = user.lastName
-        let email = user.email
-        console.log(typeof firstName)
-        res.render('account',{
-          title:"User account",
-          firstName,
-          lastName,
-          email
-        })
-      }
-    })
-    .catch(err=>{console.log(err)})
+  Controllers.projectController.getUserAccountController(req,res)
 })
 // Update user account
 router.put('/account',ensureAuthenticated, (req, res) => {
-  const {firstName,lastName,email} = req.body;
-  const id = req.user._id;
-
-  User.findByIdAndUpdate({ _id: id }, { $set: { firstName, lastName, email} })
-    .then(result => {
-        let firstName = result.firstName
-        let lastName = result.lastName
-        let email = result.email
-      
-        req.flash('success_msg', 'New user saved successfully')
-        res.json({
-          statusCode:200,
-          success_msg:"User udated"
-        })
-      
-        
-    })
-    .catch(err => console.log(err))
+  Controllers.projectController.updateUserAccountController(req,res)
 })
 module.exports = router
