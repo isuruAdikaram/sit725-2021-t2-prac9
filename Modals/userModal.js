af6b57fa31ed21a53eff97d185b366c1835839c6
Isuru Adikaram <izuru775@gmail.com>
2021-09-26 19:47:52 +1000

    User modal created

--- Modals/userModal.js
+++ Modals/userModal.js
@@ -1,1 +1,34 @@
 const mongoose = require('mongoose');
+
+// Create Schema
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    },
+    resetLink:{
+        data: String,
+        default:''
+    }
+})
+
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
