a4d6781ef67426af404465e811c9a7d310fb1d79
Isuru Adikaram <izuru775@gmail.com>
2021-09-26 19:50:28 +1000

    Minor changes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const User = require('../Modals/userModal');
+const Pet = require('../Modals/petModal');
+const express = require('express');
+const bcrypt = require('bcryptjs')
+const router = express.Router()
+const Controllers = require('../controllers')
+
+
+module.exports = router;
